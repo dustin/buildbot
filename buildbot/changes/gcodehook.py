f5c3d497fb90d7049f4df042360ac06baac0cae0
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2009-09-03 20:25:42 -0300

    Make gcodehook support branches like SVNPoller. Totally untested.

--- buildbot/changes/gcodehook.py
+++ buildbot/changes/gcodehook.py
@@ -6,26 +6,12 @@
 # of the GNU General Public License, version 2 or later, incorporated herein by
 # reference.
 
-from buildbot.changes import base, changes
+from buildbot.changes import base, changes, svnpoller
 from twisted.web import server, resource
 from twisted.application import strports
 from twisted.python import log
 
 import simplejson
-
-def bbotChangeFromHookData(revision):
-    files = []
-    files.extend(revision['added'])
-    files.extend(revision['modified'])
-    files.extend(revision['removed'])
-    return changes.Change(
-        who = revision['author'],
-        files = files,
-        comments = revision['message'],
-        revision = revision['revision'],
-        revlink = revision['url'],
-        when = revision['timestamp']
-    )
 
 class HookHandler(resource.Resource):
     """
@@ -34,9 +20,9 @@ class HookHandler(resource.Resource):
     """
     isLeaf = True
 
-    def __init__(self, addChangeFunc):
+    def __init__(self, gotChangesFunc):
         resource.Resource.__init__(self)
-        self.addChange = addChangeFunc
+        self.gotChanges = gotChangesFunc
 
     def render_GET(self, req):
         req.setHeader("Content-Type", "text/plain")
@@ -51,9 +37,9 @@ class HookHandler(resource.Resource):
             req.setResponseCode(400)
             return "Couldn't parse JSON data. Error message: %s" % e.message
 
-        for revision in data["revisions"]:
-            change = bbotChangeFromHookData(revision)
-            self.addChange(change)
+        f = getattr(self, "gotChanges")
+        f(data["revisions"])
+
 
         req.setResponseCode(200)
         req.setHeader("Content-Type", "text/plain")
@@ -63,16 +49,41 @@ class HookHandler(resource.Resource):
 class GoogleCodeHook(base.ChangeSource):
     """
     TODO
+
+    split_file: a function that is called with a string of the form
+    (BRANCH)/(FILEPATH) and should return a tuple (BRANCH, FILEPATH). This
+    function should match your repository's branch-naming policy. Each changed
+    file has a fully-qualified URL that can be split into a prefix (which equals
+    the value of the 'svnurl' argument) and a suffix; it is this suffix which is
+    passed to the split_file function.
+
+    If the function returns None, the file is ignored. Use this to indicate
+    that the file is not a part of this project.
+
+    Useful implementations of split_file functions are available in the
+    svnpoller module.
+
+    The default of split_file= is None, which indicates that no splitting should
+    be done. This is equivalent to the following function::
+
+     return (None, path)
     """
-    def __init__(self, http_port=None):
+    def __init__(self, http_port=None, split_file=None):
         if type(http_port) is int:
             http_port = "tcp:%d" % http_port
         self.http_port = http_port
 
         self.handler = resource.Resource()
-        self.handler.putChild("gcode-hook", HookHandler(self.addChange))
+        self.handler.putChild("gcode-hook", HookHandler(self.gotChanges))
         self.site = server.Site(self.handler)
         self.websrv = strports.service(self.http_port, self.site)
+        self.split_file_function = split_file or svnpoller.split_file_alwaystrunk
+
+    def split_file(self, path):
+        # use getattr() to avoid turning this function into a bound method,
+        # which would require it to have an extra 'self' argument
+        f = getattr(self, "split_file_function")
+        return f(path)
 
     def startService(self):
         self.websrv.startService()
@@ -85,4 +96,35 @@ class GoogleCodeHook(base.ChangeSource):
     def addChange(self, change):
         log.msg("addChange called")
         self.parent.addChange(change)
+
+    def gotChanges(self, revisions):
+        for revision in revisions:
+            for change in self.changesFromHookData(revision):
+                self.addChange(change)
+
+    def bbotChangesFromHookData(revision):
+        files = []
+        files.extend(revision['added'])
+        files.extend(revision['modified'])
+        files.extend(revision['removed'])
+        branches = {}
+
+        for file in files:
+            where = self.split_file(file)
+            if where:
+                branch, filename = where
+                if not branch in branches:
+                    branches[branch] = {'files':[]}
+                branches[branch]['files'].append(filename)
+
+        for branch in branches.keys():
+            yield changes.Change(
+                who = revision['author'],
+                files = branches[branch]['files'],
+                branch = branch,
+                comments = revision['message'],
+                revision = revision['revision'],
+                revlink = revision['url'],
+                when = revision['timestamp']
+            )
 
